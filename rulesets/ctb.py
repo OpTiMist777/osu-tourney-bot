2a95ba91d48d24b165ea371cb3f482d39b585a05
OpTiMist777 <77244484+OpTiMist777@users.noreply.github.com>
2026-09-20 20:48:58 +0300

    Align CTB DT minimum with official pool rules

--- rulesets/ctb.py
+++ rulesets/ctb.py
@@ -3,7 +3,7 @@ from .base import ModeRules
 CTB_RULES = ModeRules(
     mode="ctb", osu_ruleset="fruits",
     categories={"nm": "NoMod", "hd": "Hidden", "hr": "HardRock", "dt": "DoubleTime", "fm": "FreeMod", "tb": "Tiebreaker"},
-    minimums={"nm": 3, "hd": 2, "hr": 2, "dt": 3, "tb": 1},
+    minimums={"nm": 3, "hd": 2, "hr": 2, "dt": 2, "tb": 1},
     optional_categories=("fm",),
     category_order=("nm", "hd", "hr", "dt", "fm", "tb"),
     slot_mods={"nm": (), "hd": ("HD",), "hr": ("FreeMod",), "dt": ("DT", "FreeMod"), "fm": ("FreeMod",), "tb": ("FreeMod",)},
